f825d35aec32da81f3a6700d0d3f157dfa17126d
Priyansh Shah <shah.priyansh04@gmail.com>
2022-06-17 11:17:44 +0530

    Connected the login form to react-hook-form

--- app/auth/components/LoginForm.tsx
+++ app/auth/components/LoginForm.tsx
@@ -3,14 +3,28 @@ import login from 'app/auth/mutations/login'
 import { FaTwitter } from 'react-icons/fa'
 import { FcGoogle } from 'react-icons/fc'
 import { BsTwitter } from 'react-icons/bs'
+import { SubmitHandler, useForm } from 'react-hook-form'
 
 type LoginFormProps = {
   onSuccess?: (user: PromiseReturnType<typeof login>) => void
 }
 
+type InputProps = {
+  username: string
+  password: string
+}
+
 export const LoginForm = (props: LoginFormProps) => {
   const [loginMutation] = useMutation(login)
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<InputProps>()
+
+  const onSubmit: SubmitHandler<InputProps> = (data: InputProps) => console.log(data)
+  console.log(errors)
   return (
     <div className="h-full flex items-center flex-1 bg-black px-10 ">
       <div className="text-white h-4/5 flex-1 ">
@@ -41,30 +55,44 @@ export const LoginForm = (props: LoginFormProps) => {
             <div className="flex-grow border-t border-gray-400"></div>
           </div>
 
-          <div className={'flex items-center bg-white  w-full py-2 justify-center rounded-full '}>
-            <input
-              type="text"
-              placeholder={'Username'}
-              className={'flex-1 mx-4 text-black bg-transparent outline-none'}
-            />
-          </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className={'flex items-center bg-white  w-full py-2 justify-center rounded-full '}>
+              <input
+                type="text"
+                placeholder={'Username'}
+                {...register('username', { required: true, minLength: 5 })}
+                className={'flex-1 mx-4 text-black bg-transparent outline-none'}
+              />
+            </div>
 
-          <div
-            className={'flex items-center mt-4 bg-white  w-full py-2 justify-center rounded-full '}
-          >
-            <input
-              type="password"
-              placeholder={'Password'}
-              className={'flex-1 mx-4 text-black bg-transparent outline-none'}
-            />
-          </div>
-          <button
-            className={
-              'flex items-center mt-6 bg-white text-white bg-twitter w-full py-2 justify-center rounded-full transition-transform duration-250 ease-out active:scale-90 hover:opacity-90'
-            }
-          >
-            Login
-          </button>
+            {errors.username?.type === 'required' && (
+              <p className={'ml-2 text-xs text-red-700'}>Username is required</p>
+            )}
+            {errors.username?.type === 'minLength' && (
+              <p className={'ml-2 text-xs text-red-700'}>Minimum length of username is 5</p>
+            )}
+            <div
+              className={
+                'flex items-center mt-4 bg-white  w-full py-2 justify-center rounded-full '
+              }
+            >
+              <input
+                type="password"
+                {...register('password', { required: true })}
+                placeholder={'Password'}
+                className={'flex-1 mx-4 text-black bg-transparent outline-none'}
+              />
+            </div>
+            {errors.password && <p className={'ml-2 text-xs text-red-700'}>Password is required</p>}
+
+            <button
+              className={
+                'flex items-center mt-6 bg-white text-white bg-twitter w-full py-2 justify-center rounded-full transition-transform duration-250 ease-out active:scale-90 hover:opacity-90'
+              }
+            >
+              Login
+            </button>
+          </form>
           <p className={'text-xs ml-2 mt-2'}>
             By signing up, you agree to the{' '}
             <span className={'text-twitter cursor-pointer'}>Terms of Service</span> and{' '}
